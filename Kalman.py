11d51ce58b1716299f1c2460291d5e66d8dc60b0
rocheparadox <rocheinsid@gmail.com>
2018-07-12 09:39:06 +0530

    Code Changes on library file

--- Kalman.py
+++ Kalman.py
@@ -1,14 +1,14 @@
 class Kalman:
     def __init__():
-        this.QAngle = 0.0
-        this.QBias = 0.0
-        this.RMeasure = 0.0
+        this.QAngle = 0.001
+        this.QBias = 0.003
+        this.RMeasure = 0.03
         this.angle = 0.0
         this.bias = 0.0
         this.rate = 0.0
-        p=[[0.0,0.0],[0.0,0.0]]
-        
-    def kalman():
+        this.p=[[0.0,0.0],[0.0,0.0]]
+
+    '''def kalman():
         QAngle = 0.001
         QBias = 0.003
         RMeasure = 0.03
@@ -19,33 +19,41 @@ class Kalman:
         p[0][0] = 0.0
         p[0][1] = 0.0
         p[1][0] = 0.0
-        p[1][1] = 0.0
+        p[1][1] = 0.0'''
 
     def getAngle(newAngle, newRate,dt):
-        rate = newRate - bias;
-        angle += dt * rate;
+        #step 1:
+        this.rate = newRate - this.bias;    #new_rate is the latest Gyro measurement
+        this.angle += dt * this.rate;
 
-        p[0][0] += dt * (dt*p[1][1] - p[0][1] -p[1][0] + QAngle)
-        p[0][1] -= dt * p[1][1]
-        p[1][0] -= dt * p[1][1]
-        p[1][1] += QBias * dt
+        #Step 2:
+        this.p[0][0] += dt * (dt*this.p[1][1] -this.p[0][1] - this.p[1][0] + this.QAngle)
+        this.p[0][1] -= dt * this.p[1][1]
+        this.p[1][0] -= dt * this.p[1][1]
+        this.p[1][1] += this.QBias * dt
 
-        s = p[0][0] + RMeasure
-        k[0] = p[0][0]/s
-        k[1] = p[1][0]/s
+        #Step 3: Innovation
+        y = newAngle - this.angle
 
-        y = newAngle - angle
+        #Step 4: Innovation covariance
+        s = this.p[0][0] + this.RMeasure
 
+        #Step 5:    Kalman Gain
+        k[0] = this.p[0][0]/s
+        k[1] = this.p[1][0]/s
+
+        #Step 6: Update the Angle
         angle += k[0] * y
-        bias  += k[1] * y
+        this.bias  += k[1] * y
 
-        p00Temp = p[0][0]
-        p01Temp = p[0][1]
+        #Step 7: Calculate estimation error covariance - Update the error covariance
+        p00Temp = this.p[0][0]
+        p01Temp = this.p[0][1]
 
-        P[0][0] -= K[0] * P00Temp;
-        P[0][1] -= K[0] * P01Temp;
-        P[1][0] -= K[1] * P00Temp;
-        P[1][1] -= K[1] * P01Temp;
+        this.P[0][0] -= K[0] * P00Temp;
+        this.P[0][1] -= K[0] * P01Temp;
+        this.P[1][0] -= K[1] * P00Temp;
+        this.P[1][1] -= K[1] * P01Temp;
 
         return Angle
 
@@ -68,7 +76,7 @@ class Kalman:
         return this.QAngle
 
     def getQBias():
-        return QBias
+        return this.QBias
 
     def  getRMeasure():
         return this.RMeasure
