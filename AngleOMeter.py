07363738660c14b81d21425201c7264b4f8ef0c9
rocheparadox <rocheinside@gmail.com>
2018-10-04 22:26:27 +0530

    Error handling

--- AngleOMeter.py
+++ AngleOMeter.py
@@ -92,69 +92,76 @@ compAngleX = roll;
 compAngleY = pitch;
 
 timer = time.time()
-
+flag = 0
 while True:
-    #Read Accelerometer raw value
-    accX = read_raw_data(ACCEL_XOUT_H)
-    accY = read_raw_data(ACCEL_YOUT_H)
-    accZ = read_raw_data(ACCEL_ZOUT_H)
+	if(flag >5): #Problem with the connection
+		print("There is a problem with the connection")
+		break
+	try:
+	    #Read Accelerometer raw value
+	    accX = read_raw_data(ACCEL_XOUT_H)
+	    accY = read_raw_data(ACCEL_YOUT_H)
+	    accZ = read_raw_data(ACCEL_ZOUT_H)
 
-    #Read Gyroscope raw value
-    gyroX = read_raw_data(GYRO_XOUT_H)
-    gyroY = read_raw_data(GYRO_YOUT_H)
-    gyroZ = read_raw_data(GYRO_ZOUT_H)
+	    #Read Gyroscope raw value
+	    gyroX = read_raw_data(GYRO_XOUT_H)
+	    gyroY = read_raw_data(GYRO_YOUT_H)
+	    gyroZ = read_raw_data(GYRO_ZOUT_H)
 
-    dt = time.time() - timer
-    timer = time.time()
+	    dt = time.time() - timer
+	    timer = time.time()
 
-    if (RestrictPitch):
-        roll = math.atan2(accY,accZ) * radToDeg
-        pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
-    else:
-        roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
-        pitch = math.atan2(-accX,accZ) * radToDeg
+	    if (RestrictPitch):
+	        roll = math.atan2(accY,accZ) * radToDeg
+	        pitch = math.atan(-accX/math.sqrt((accY**2)+(accZ**2))) * radToDeg
+	    else:
+	        roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
+	        pitch = math.atan2(-accX,accZ) * radToDeg
 
-    gyroXRate = gyroX/131
-    gyroYRate = gyroY/131
+	    gyroXRate = gyroX/131
+	    gyroYRate = gyroY/131
 
-    if (RestrictPitch):
+	    if (RestrictPitch):
 
-        if((roll < -90 and kalAngleX >90) or (roll > 90 and kalAngleX < -90)):
-            kalmanX.setAngle(roll)
-            complAngleX = roll
-            kalAngleX   = roll
-            gyroXAngle  = roll
-        else:
-            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
+	        if((roll < -90 and kalAngleX >90) or (roll > 90 and kalAngleX < -90)):
+	            kalmanX.setAngle(roll)
+	            complAngleX = roll
+	            kalAngleX   = roll
+	            gyroXAngle  = roll
+	        else:
+	            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
 
-        if(abs(kalAngleX)>90):
-            gyroYRate  = -gyroYRate
-            kalAngleY  = kalmanY.getAngle(pitch,gyroYRate,dt)
-    else:
+	        if(abs(kalAngleX)>90):
+	            gyroYRate  = -gyroYRate
+	            kalAngleY  = kalmanY.getAngle(pitch,gyroYRate,dt)
+	    else:
 
-        if((pitch < -90 and kalAngleY >90) or (pitch > 90 and kalAngleY < -90)):
-            kalmanY.setAngle(pitch)
-            complAngleY = pitch
-            kalAngleY   = pitch
-            gyroYAngle  = pitch
-        else:
-            kalAngleY = kalmanY.getAngle(pitch,gyroYRate,dt)
+	        if((pitch < -90 and kalAngleY >90) or (pitch > 90 and kalAngleY < -90)):
+	            kalmanY.setAngle(pitch)
+	            complAngleY = pitch
+	            kalAngleY   = pitch
+	            gyroYAngle  = pitch
+	        else:
+	            kalAngleY = kalmanY.getAngle(pitch,gyroYRate,dt)
 
-        if(abs(kalAngleY)>90):
-            gyroXRate  = -gyroXRate
-            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
+	        if(abs(kalAngleY)>90):
+	            gyroXRate  = -gyroXRate
+	            kalAngleX = kalmanX.getAngle(roll,gyroXRate,dt)
 
-    gyroXAngle = gyroXRate * dt
-    gyroYAngle = gyroYAngle * dt
+	    gyroXAngle = gyroXRate * dt
+	    gyroYAngle = gyroYAngle * dt
 
-    compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
-    compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
+	    compAngleX = 0.93 * (compAngleX + gyroXRate * dt) + 0.07 * roll
+	    compAngleY = 0.93 * (compAngleY + gyroYRate * dt) + 0.07 * pitch
 
-    if ((gyroXAngle < -180) or (gyroXAngle > 180)):
-        gyroXAngle = kalAngleX
-    if ((gyroYAngle < -180) or (gyroYAngle > 180)):
-        gyroYAngle = kalAngleY
+	    if ((gyroXAngle < -180) or (gyroXAngle > 180)):
+	        gyroXAngle = kalAngleX
+	    if ((gyroYAngle < -180) or (gyroYAngle > 180)):
+	        gyroYAngle = kalAngleY
 
-    print("Angle X: " + str(kalAngleX)+"   " +"Angle Y: " + str(kalAngleY))
-    #print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
-    time.sleep(0.005)
+	    print("Angle X: " + str(kalAngleX)+"   " +"Angle Y: " + str(kalAngleY))
+	    #print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
+	    time.sleep(0.005)
+
+	except (Exception exc):
+		flag += 1
